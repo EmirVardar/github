cfb56a562e0e7f72030da815cbbe524f952bb925
Emir Vardar <emir.vardar38@gmail.com>
2024-11-30 13:39:00 +0300

    emir cshtml vievs klasörüne eklendi

--- github/Controllers/HomeController.cs
+++ github/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace github.Controllers
         {
             return View();
         }
+        public IActionResult Emir()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
