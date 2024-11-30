2806b1049551dca1a12bbe959d16400569deaac5
Emir Vardar <121385791+EmirVardar@users.noreply.github.com>
2024-11-30 13:54:55 +0300

    emir2github üzerinden değişiklik yapıldı

--- github/Controllers/HomeController.cs
+++ github/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace github.Controllers
         {
             return View();
         }
+        public IActionResult Emir2()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
